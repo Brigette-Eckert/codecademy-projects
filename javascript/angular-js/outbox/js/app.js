2128c0b310a8a178730cc9cdecb2883b69806876
Brigette Eckert <brigette.eckert@live.com>
2015-09-09 13:32:03 -0700

    started working on outbox2 project, adding routing to project to display email contents

--- javascript/angular-js/outbox/js/app.js
+++ javascript/angular-js/outbox/js/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('OutboxApp', []);
+var app = angular.module('OutboxApp', ['ngRoute']);
+app.config(function($routeProvider) {
+			$routeProvider
+				.when('/outbox', {
+					controller: 'HomeController',
+					templateURL: 'views/home.html'
+				})
+				.when('/outbox/:id', {
+						controller: 'EmailController',
+						templateURL: 'views/email.html'
+					})
+				.otherwise({
+					redirectTo: '/outbox'
+				});
+
+			}); 
