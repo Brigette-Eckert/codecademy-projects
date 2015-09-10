5cdce14ba055fd4aa16f0f7072478efd81f0fc3d
Brigette Eckert <brigette.eckert@live.com>
2015-09-09 18:48:22 -0700

    finished outbox2 so routing now works and info displays when email is clicked on

--- javascript/angular-js/outbox/js/app.js
+++ javascript/angular-js/outbox/js/app.js
@@ -3,11 +3,11 @@ app.config(function($routeProvider) {
 			$routeProvider
 				.when('/outbox', {
 					controller: 'HomeController',
-					templateURL: 'views/home.html'
+					templateUrl: 'views/home.html'
 				})
 				.when('/outbox/:id', {
 						controller: 'EmailController',
-						templateURL: 'views/email.html'
+						templateUrl: 'views/email.html'
 					})
 				.otherwise({
 					redirectTo: '/outbox'
